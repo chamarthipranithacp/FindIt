0d5e3ad3bb92d6bd346d819b51063268c5444fcd
chamarthipranithacp <chamarthipranithacp@gmail.com>
2026-07-23 11:36:38 +0530

    background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -39,6 +39,7 @@ const ProtectedRoute = ({ children }) => {
 function App() {
   return (
     <ThemeProvider>
+      <div className="bg-moving-layer" />
       <AuthProvider>
         <BrowserRouter>
           <Routes>
